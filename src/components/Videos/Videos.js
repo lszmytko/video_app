e9069d4e3e7c889cab1d2cd1e73725e3c6d2a664
Łukasz Szmytko <lukasz.szmytko@grupawp.pl>
2022-06-03 16:21:33 +0200

    remove all pres components

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { useGlobalContext } from "../../context/context";
-
-import VideosPres from "./VideosPres";
+import { Video, VideoModal } from "../components";
+import { Container, Row } from "reactstrap";
 
 const Videos = () => {
   const { favourite, isFavouriteShown, page, paginatedVideos, loading } =
     useGlobalContext();
-  return (
-    <VideosPres
-      favourite={favourite}
-      isFavouriteShown={isFavouriteShown}
-      page={page}
-      paginatedVideos={paginatedVideos}
-      loading={loading}
-    />
-  );
+
+    if (loading) return <div className="loader"></div>;
+    return (
+      <Container className="video-container">
+        <Row xs="justify-content-center" className="video-row">
+          {isFavouriteShown
+            ? favourite.map((item, index) => {
+                return <Video {...item} key={index} />;
+              })
+            : paginatedVideos[page].map((item, index) => {
+                return <Video {...item} key={index} />;
+              })}
+        </Row>
+        <VideoModal />
+      </Container>
+    );
+
 };
 
 export default Videos;
